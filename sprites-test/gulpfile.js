5ccaf010fbd882475576a74e59159eeb111b066a
missnote <missnote.cn@gmail.com>
2018-08-06 02:02:22 +0800

    init

---
+++ sprites-test/gulpfile.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var buffer = require('vinyl-buffer');
+var merge = require('merge-stream');
+
+gulp.task('sprite', function () {
+    console.log(plugins);
+    var spriteData = gulp.src('./src/images/*.png').pipe(plugins.spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.scss'
+    }));
+    var imgStream = spriteData.img
+        .pipe(buffer())
+        .pipe(gulp.dest('./src/dest/images/'));
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('./src/dest/css/'));
+    return merge(imgStream, cssStream);
+})
