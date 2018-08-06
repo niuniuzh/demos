3f79a4467b46f2c186814b8389810d89a85464a5
missnote <missnote.cn@gmail.com>
2018-08-07 02:19:26 +0800

    kengdie

--- sprites-test/gulpfile.js
+++ sprites-test/gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var buffer = require('vinyl-buffer');
 var merge = require('merge-stream');
+var sass = require('gulp-sass');
 
 gulp.task('sprite', function () {
     console.log(plugins);
@@ -20,8 +21,39 @@ gulp.task('sprite', function () {
     return merge(imgStream, cssStream);
 })
 
-gulp.task('watch',function(){
-    gulp.watch('src/images/*.png',['sprite']);
+gulp.task('watch', function () {
+    gulp.watch('src/images/*.png', ['sprite']);
 })
 
-gulp.task('default',['sprite'])
+gulp.task('default', ['sprite'])
+
+gulp.task('sprite-scss', function () {
+    var spriteData = gulp.src('./src/images/*.png').pipe(plugins.spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.scss',
+        cssTemplate: './handlebarsStr.hbs',
+        imgPath: '../images/' + 'sprite.png'
+    }));
+    var imgStream = spriteData.img
+        .pipe(buffer())
+        .pipe(gulp.dest('./src/dest/images/'));
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('./src/dest/css/'));
+    return merge(imgStream, cssStream);
+})
+
+gulp.task('sass', function () {
+    return gulp.src('./src/dest/css/*.scss')
+    .pipe(sass({
+        sourceComments: 'map',
+        outputStyle: 'nested'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./src/dest/css'));
+})
+
+gulp.task('sass:watch', function () {
+    // gulp.watch('src/images/*.png', ['sprite-scss']);
+    gulp.watch('src/dest/css/*.scss', ['sass']);
+});
+
+gulp.task('sass-watch', ['sass', 'sass:watch', 'sprite-scss'])
